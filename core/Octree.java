f5a074adb03801708093edb5fc5083336aab012b
vanshikas20 <vanshikas20105@gmail.com>
2025-04-17 23:49:21 +0530

    Update Octree.java

--- core/Octree.java
+++ core/Octree.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Octree {
     private AABB bounds;
-    private List<GameObject> objects;
-    private Octree[] children;
-    private boolean isDivided = false;
+    private List<GameObject> objects; //stores the blue boxes
+    private Octree[] children;//subdivisions of transparent outer box
+    private boolean isDivided = false;//flag which checks if the bound is subdivided
 
     public Octree(AABB bounds) {
         this.bounds = bounds;
@@ -25,8 +25,9 @@ public class Octree {
         }
     }
 
-    private void subdivide() {
-        double midX = (bounds.minX + bounds.maxX) / 2;
+    private void subdivide() { //divides space into octant when no of boxes in particular
+        // octant are more than 4
+        double midX = (bounds.minX + bounds.maxX) / 2;//calculating centre of the cube  (com)
         double midY = (bounds.minY + bounds.maxY) / 2;
         double midZ = (bounds.minZ + bounds.maxZ) / 2;
     
@@ -41,7 +42,7 @@ public class Octree {
         children[6] = new Octree(new AABB(bounds.minX, midY, midZ, midX, bounds.maxY, bounds.maxZ));
         children[7] = new Octree(new AABB(midX, midY, midZ, bounds.maxX, bounds.maxY, bounds.maxZ));
         
-        isDivided = true;
+        isDivided = true;//flag that stores if particular octant was subdivided or not
     }
 
     public List<GameObject> query(AABB range) {
